2b2131f760b1f49f99cf835ba0d9471832f22b11
venjiang <venjiang@gmail.com>
2022-07-20 16:55:53 +0800

    edge func

---
+++ pages/api/edge.ts
@@ -0,0 +1,19 @@
+import {NextRequest, NextResponse} from 'next/server';
+
+export default (req: NextRequest) => {
+    // console.log('Edit and run at the edge!')
+    // const geo = req.geo()
+    // const country = geo.country || 'none-country'
+    // const city = geo.city || 'none-city'
+    // const region = geo.city || 'none-region'
+
+    // return NextResponse(` ip: ${req.ip}, geo.country: ${country}, geo.city: ${city}, geo.region: ${region}`)
+    return NextResponse.json({
+        name: `Hello, from ${req.url} I'm now an Edge Function!`,
+        req: JSON.stringify(req),
+    });
+};
+
+export const config = {
+    runtime: 'experimental-edge',
+};
